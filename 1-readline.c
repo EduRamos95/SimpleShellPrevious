5b375dd6a01ff82767e6479cf210a72a16fede4f
EduRamos95 <eduramos08.30@gmail.com>
2022-08-02 22:13:07 -0500

    changes

--- 1-readline.c
+++ 1-readline.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-
+	int n = 0;
 	/*ssize_t getline (char **restrict lineptr, size_t *restrict n, FILE *restrict stream);*/
 	while (1)
 	{
@@ -14,7 +14,12 @@ int main(void)
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 			break;
-		printf("%s",line);
+		/*se incrementa un caracter ENTER*/
+		n = strlen(line);
+		/* /bin/ls_-l_-a\0 -->13 sin nulo*/
+		printf("line->_%s_(ENTER)\n",line);
+		printf("n->_%d\n", n);
+		printf("read->_%ld\n", read);
 	}
 	free(line);
 	return (0);
